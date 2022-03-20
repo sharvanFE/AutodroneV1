663d6ef147f4d78e46745c8c1301362904138c2e
karthikece <karthikece@users.noreply.github.com>
2022-03-20 21:16:25 +0530

    Update RX.ino

--- RX.ino
+++ RX.ino
@@ -5,7 +5,7 @@ const static uint8_t RADIO_ID = 1;             // Our radio's id.
 const static uint8_t PIN_RADIO_CE = 9;
 const static uint8_t PIN_RADIO_CSN = 10;
 
-
+// struct
 struct RadioPacket {
   uint8_t FromRadioId;
   uint32_t x1;
